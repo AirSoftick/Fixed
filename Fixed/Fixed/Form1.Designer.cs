7d551b3b661c2b3c927427a393e066236b9e6a65
AirSoftick <141844045+AirSoftick@users.noreply.github.com>
2023-08-09 18:44:51 +0300

    Fixed

---
+++ Fixed/Fixed/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Fixed
+{
+    partial class FixedForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FixCaps = new System.Windows.Forms.Button();
+            this.TranslateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FixCaps
+            // 
+            this.FixCaps.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.FixCaps.Font = new System.Drawing.Font("Oldtimer", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FixCaps.ForeColor = System.Drawing.Color.White;
+            this.FixCaps.Location = new System.Drawing.Point(13, 13);
+            this.FixCaps.Margin = new System.Windows.Forms.Padding(4);
+            this.FixCaps.Name = "FixCaps";
+            this.FixCaps.Size = new System.Drawing.Size(235, 44);
+            this.FixCaps.TabIndex = 0;
+            this.FixCaps.Text = "FixCaps";
+            this.FixCaps.UseVisualStyleBackColor = false;
+            this.FixCaps.Click += new System.EventHandler(this.FixCaps_Click);
+            // 
+            // TranslateButton
+            // 
+            this.TranslateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.TranslateButton.Font = new System.Drawing.Font("Oldtimer", 10.8F);
+            this.TranslateButton.ForeColor = System.Drawing.Color.White;
+            this.TranslateButton.Location = new System.Drawing.Point(13, 65);
+            this.TranslateButton.Margin = new System.Windows.Forms.Padding(4);
+            this.TranslateButton.Name = "TranslateButton";
+            this.TranslateButton.Size = new System.Drawing.Size(235, 44);
+            this.TranslateButton.TabIndex = 1;
+            this.TranslateButton.Text = "FixTranslate";
+            this.TranslateButton.UseVisualStyleBackColor = false;
+            this.TranslateButton.Click += new System.EventHandler(this.TranslateButton_Click);
+            // 
+            // FixedForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(261, 125);
+            this.Controls.Add(this.TranslateButton);
+            this.Controls.Add(this.FixCaps);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FixedForm";
+            this.Text = "Fixed";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FixCaps;
+        private System.Windows.Forms.Button TranslateButton;
+    }
+}
+
---
+++ Fixed/Fixed/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fixed
+{
+    public partial class FixedForm : Form
+    {
+        public FixedForm()
+        {
+            InitializeComponent();
+        }
+
+        // Исправление больших и маленьких букв
+        private void FixCaps_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                string text = Clipboard.GetText();
+                string formattedText = FormatText(text);
+                Clipboard.SetText(formattedText);
+
+                NotifyIcon notifyIcon = new NotifyIcon();
+                notifyIcon.Icon = SystemIcons.Information;
+                notifyIcon.Visible = true;
+                notifyIcon.BalloonTipTitle = "";
+                notifyIcon.BalloonTipText = ": ) " + "\nТекст исправлен и скопирован в буфер обмена.";
+                notifyIcon.ShowBalloonTip(2000);
+            }
+            else
+            {
+                NotifyIcon notifyIcon = new NotifyIcon();
+                notifyIcon.Icon = SystemIcons.Information;
+                notifyIcon.Visible = true;
+                notifyIcon.BalloonTipTitle = "";
+                notifyIcon.BalloonTipText = "Упс, Problem : (" + "\nУбедитесь, что ВЫ скопировали текст." + "\nПроверить можно зажав [ win + v ]";
+                notifyIcon.ShowBalloonTip(2000);
+            }
+        }
+
+        private string FormatText(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            StringBuilder formattedText = new StringBuilder(text.ToLower());
+            bool capitalizeNext = true;
+
+            for (int i = 0; i < formattedText.Length; i++)
+            {
+                char currentChar = formattedText[i];
+
+                if (capitalizeNext && char.IsLetter(currentChar))
+                {
+                    formattedText[i] = char.ToUpper(currentChar);
+                    capitalizeNext = false;
+                }
+                else if (currentChar == '!' || currentChar == '?' || currentChar == ';' || currentChar == '.')
+                {
+                    capitalizeNext = true;
+                }
+            }
+
+            return formattedText.ToString();
+        }
+
+        // Перевод текста
+        private void TranslateButton_Click(object sender, EventArgs e)
+        {
+            string clipboardText = Clipboard.GetText();
+
+            string decodedText = DecodeText(clipboardText);
+
+            if (!string.IsNullOrEmpty(decodedText))
+            {
+                Clipboard.SetText(decodedText);
+
+                NotifyIcon notifyIcon = new NotifyIcon();
+                notifyIcon.Icon = SystemIcons.Information;
+                notifyIcon.Visible = true;
+                notifyIcon.BalloonTipTitle = "";
+                notifyIcon.BalloonTipText = ": ) " + "\nТекст переведен и скопирован в буфер обмена.";
+                notifyIcon.ShowBalloonTip(2000);
+            }
+            else
+            {
+                NotifyIcon notifyIcon = new NotifyIcon();
+                notifyIcon.Icon = SystemIcons.Information;
+                notifyIcon.Visible = true;
+                notifyIcon.BalloonTipTitle = "";
+                notifyIcon.BalloonTipText = "Упс, Problem : (" + "\nУбедитесь, что ВЫ скопировали текст." + "\nПроверить можно зажав [ win + v ]";
+                notifyIcon.ShowBalloonTip(2000);
+            }
+        }
+
+        private string DecodeText(string text)
+        {
+            StringBuilder decodedText = new StringBuilder();
+
+            // Определяем язык текста на основе первой буквы
+            char firstCharacter = text.FirstOrDefault();
+            bool isRussian = IsRussianCharacter(firstCharacter);
+
+            foreach (char c in text)
+            {
+                if (isRussian)
+                {
+                    decodedText.Append(DecodeToEnglishCharacter(c));
+                }
+                else
+                {
+                    decodedText.Append(DecodeToRussianCharacter(c));
+                }
+            }
+
+            return decodedText.ToString();
+        }
+
+        // Маппинг символов для декодирования с русского на английский
+        private Dictionary<char, char> russianToEnglishCharacterMap = new Dictionary<char, char>()
+        {
+            {'q', 'й'}, {'w', 'ц'}, {'e', 'у'}, {'r', 'к'},
+            {'t', 'е'}, {'y', 'н'}, {'u', 'г'}, {'i', 'ш'},
+            {'o', 'щ'}, {'p', 'з'}, {'[', 'х'}, {']', 'ъ'},
+            {'a', 'ф'}, {'s', 'ы'}, {'d', 'в'}, {'f', 'а'},
+            {'g', 'п'}, {'h', 'р'}, {'j', 'о'}, {'k', 'л'},
+            {'l', 'д'}, {';', 'ж'}, {'\'', 'э'}, {'z', 'я'},
+            {'x', 'ч'}, {'c', 'с'}, {'v', 'м'}, {'b', 'и'},
+            {'n', 'т'}, {'m', 'ь'}, {',', 'б'}, {'.', 'ю'},
+            {'/', '.'}, {'`', 'ё'}, {'Q', 'Й'}, {'W', 'Ц'},
+            {'E', 'У'}, {'R', 'К'}, {'T', 'Е'}, {'Y', 'Н'},
+            {'U', 'Г'}, {'I', 'Ш'}, {'O', 'Щ'}, {'P', 'З'},
+            {'{', 'Х'}, {'}', 'Ъ'}, {'A', 'Ф'}, {'S', 'Ы'},
+            {'D', 'В'}, {'F', 'А'}, {'G', 'П'}, {'H', 'Р'},
+            {'J', 'О'}, {'K', 'Л'}, {'L', 'Д'}, {':', 'Ж'},
+            {'"', 'Э'}, {'Z', 'Я'}, {'X', 'Ч'}, {'C', 'С'},
+            {'V', 'М'}, {'B', 'И'}, {'N', 'Т'}, {'M', 'Ь'},
+            {'<', 'Б'}, {'>', 'Ю'}, {'?', ','}, {'&', '?'}
+        };
+
+        // Маппинг символов для декодирования с английского на русский
+        private Dictionary<char, char> englishToRussianCharacterMap = new Dictionary<char, char>()
+        {
+            {'й', 'q'}, {'ц', 'w'}, {'у', 'e'}, {'к', 'r'},
+            {'е', 't'}, {'н', 'y'}, {'г', 'u'}, {'ш', 'i'},
+            {'щ', 'o'}, {'з', 'p'}, {'х', '['}, {'ъ', ']'},
+            {'ф', 'a'}, {'ы', 's'}, {'в', 'd'}, {'а', 'f'},
+            {'п', 'g'}, {'р', 'h'}, {'о', 'j'}, {'л', 'k'},
+            {'д', 'l'}, {'ж', ';'}, {'э', '\''}, {'я', 'z'},
+            {'ч', 'x'}, {'с', 'c'}, {'м', 'v'}, {'и', 'b'},
+            {'т', 'n'}, {'ь', 'm'}, {',', ','}, {'.', '.'},
+            {'/', '/'}, {'ё', '`'}, {'Й', 'Q'}, {'Ц', 'W'},
+            {'У', 'E'}, {'К', 'R'}, {'Е', 'T'}, {'Н', 'Y'},
+            {'Г', 'U'}, {'Ш', 'I'}, {'Щ', 'O'}, {'З', 'P'},
+            {'Х', '{'}, {'Ъ', '}'}, {'Ф', 'A'}, {'Ы', 'S'},
+            {'В', 'D'}, {'А', 'F'}, {'П', 'G'}, {'Р', 'H'},
+            {'О', 'J'}, {'Л', 'K'}, {'Д', 'L'}, {'Ж', ':'},
+            {'Э', '"'}, {'Я', 'Z'}, {'Ч', 'X'}, {'С', 'C'},
+            {'М', 'V'}, {'И', 'B'}, {'Т', 'N'}, {'Ь', 'M'},
+            {'Б', '<'}, {'Ю', '>'}, {'?', '?'}, {'&', '&'}
+        };
+
+        private bool IsRussianCharacter(char character)
+        {
+            return russianToEnglishCharacterMap.ContainsKey(character);
+        }
+
+        private char DecodeToEnglishCharacter(char character)
+        {
+            // Декодирование символа с русского на английский
+            if (russianToEnglishCharacterMap.ContainsKey(character))
+            {
+                return russianToEnglishCharacterMap[character];
+            }
+            else
+            {
+                return character;
+            }
+        }
+
+        private char DecodeToRussianCharacter(char character)
+        {
+            // Декодирование символа с английского на русский
+            if (englishToRussianCharacterMap.ContainsKey(character))
+            {
+                return englishToRussianCharacterMap[character];
+            }
+            else
+            {
+                return character;
+            }
+        }
+    }
+}
